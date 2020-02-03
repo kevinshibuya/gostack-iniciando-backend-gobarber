5636667934bea452275941ddefba1b83be0277d7
Kevin Shibuya <kevinshibuya@hotmail.com>
2020-02-03 14:27:21 -0300

    update

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '880e2e0e8416f38a02080fb6a95a6b58',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Kevin Shibuya',
-    email: 'kevinshibuya@hotmail.com',
-    password_hash: '9312738913812',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-  return res.json(user);
-});
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
